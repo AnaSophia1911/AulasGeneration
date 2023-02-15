d5e778082adc251286855a1dbe5f1d6ccf2bddab
AnaSophia1911 <anasophiajacintosouza@gmail.com>
2023-02-15 10:57:54 -0300

    exercícios pendentes feitos no dia 15/2

---
+++ src/exercicio1Aula/exercicio6dowhile.java
@@ -0,0 +1,24 @@
+package exercicio1Aula;
+
+import java.util.Scanner;
+
+public class exercicio6dowhile {
+
+	public static void main(String[] args) {
+	
+		Scanner leia = new Scanner(System.in);
+		
+		int n,soma=0;
+		
+		do {System.out.println("\nDigite um número: ");
+		n = leia.nextInt();
+			if(n>0 && n!=0){
+				soma += n;}
+				
+			
+			
+		}while(n!=0);
+		System.out.println("\nA soma dos números positivos é de: "+soma+".");
+	}
+
+}
